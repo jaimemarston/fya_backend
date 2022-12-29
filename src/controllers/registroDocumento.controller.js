082a746dc7d7b39523c698502e8ce3b1a8f3a814
wisftock <fullstack.sw@gmail.com>
2022-12-29 18:35:36 -0500

    feat: registro documento crud

--- src/controllers/registroDocumento.controller.js
+++ src/controllers/registroDocumento.controller.js
@@ -1,53 +1,55 @@
 import { request, response } from 'express';
-import { validateLugarSolicitud } from '../helpers/lugarSolicitud.js';
-import { LugarComision } from '../models/index.js';
+import { validateRegistroDocumento } from '../helpers/schemaRegistroDocumento.js';
+import { RegistroDocumento } from '../models/index.js';
 
-const comisionOne = async (req = request, res = response) => {
+const lugarOne = async (req = request, res = response) => {
   const { id } = req.params;
   try {
-    const lugarComision = await LugarComision.findOne({
+    const registroDocumento = await RegistroDocumento.findOne({
       where: { id, estado: true },
     });
-
-    if (!lugarComision) {
+    if (!registroDocumento) {
       return res
         .status(404)
-        .json({ message: 'Lugar de comisión no encontrado' });
+        .json({ message: 'Registro de documento no encontrado' });
     }
-    res.status(200).json({ message: 'Lugar encontrado', lugarComision });
+    res
+      .status(200)
+      .json({ message: 'Registro de documento encontrado', registroDocumento });
   } catch (err) {
     return res.status(400).json({ message: 'Hable con el administrador', err });
   }
 };
 
-const comisionAll = async (req = request, res = response) => {
+const lugarAll = async (req = request, res = response) => {
   try {
     const [lugar, count] = await Promise.all([
-      LugarComision.findAll({
+      RegistroDocumento.findAll({
         order: ['id'],
         where: { estado: true },
       }),
-      LugarComision.count({
+      RegistroDocumento.count({
         where: { estado: true },
       }),
     ]);
     res
       .status(200)
-      .json({ message: 'Lista de lugares de comisión', lugar, count });
+      .json({ message: 'Lista de registro de documento', lugar, count });
   } catch (err) {
     return res.status(400).json({ message: 'Hable con el administrador', err });
   }
 };
 
-const comisionAdd = async (req = request, res = response) => {
+const lugarAdd = async (req = request, res = response) => {
   const { body } = req;
-  const { error } = validateLugarSolicitud(req.body);
+  const { error } = validateRegistroDocumento(req.body);
+
   if (error) {
     const err = error.details[0].message;
     return res.status(400).json({ message: err });
   }
   try {
-    const existCode = await LugarComision.findOne({
+    const existCode = await RegistroDocumento.findOne({
       where: { codigo: body.codigo },
     });
 
@@ -55,44 +57,41 @@ const comisionAdd = async (req = request, res = response) => {
       return res.status(404).json({ message: 'El código ya existe' });
     }
 
-    const lugarComision = await LugarComision.create({ ...body });
-    res.status(201).json({ message: 'Se ha creado con éxito', lugarComision });
+    const registroDocumento = await RegistroDocumento.create({ ...body });
+
+    res
+      .status(201)
+      .json({ message: 'Se ha creado con éxito', registroDocumento });
   } catch (err) {
     res.status(400).json({ message: 'hable con el administrador', err });
   }
 };
 
-const comisionAddAll = async (req = request, res = response) => {
+const lugarAddAll = async (req = request, res = response) => {
   const { body } = req;
-
   let historial = [];
   let existCode;
   try {
     body.forEach(async (element, index) => {
-      const { error } = validateLugarSolicitud(element);
+      const { error } = validateRegistroDocumento(element);
 
       if (error) {
-        console.log('error =>', element);
-
         historial.push(element);
       } else {
-        existCode = await LugarComision.findOne({
+        existCode = await RegistroDocumento.findOne({
           where: { codigo: element.codigo },
         });
         if (existCode) {
           console.log('existe=>', element);
-
           historial.push(element);
         } else {
           console.log('agregar =>', element);
-          await LugarComision.create({ ...element });
+          await RegistroDocumento.create({ ...element });
         }
       }
-
       if (body.length - 1 === index) {
         const unicos = [...new Set(historial)];
         let respuesta = '';
-
         if (error) {
           respuesta = 'Hubo un error, revise el documento';
           res.status(400).json({
@@ -120,24 +119,21 @@ const comisionAddAll = async (req = request, res = response) => {
   }
 };
 
-const comisionUpdate = async (req = request, res = response) => {
+const lugarUpdate = async (req = request, res = response) => {
   const { id } = req.params;
   const { body } = req;
   try {
-    const lugarComision = await LugarComision.findByPk(id);
-    const existCode = await LugarComision.findOne({
+    const registroDocumento = await RegistroDocumento.findByPk(id);
+    const existCode = await RegistroDocumento.findOne({
       where: { codigo: body.codigo },
     });
-
     if (existCode) {
       return res.status(404).json({ message: 'El código ya existe' });
     }
-
-    if (!lugarComision) {
+    if (!registroDocumento) {
       return res.status(404).json({ message: 'El dato ingresado no existe' });
     }
-
-    await LugarComision.update(
+    await RegistroDocumento.update(
       { ...body },
       {
         where: {
@@ -145,10 +141,9 @@ const comisionUpdate = async (req = request, res = response) => {
         },
       }
     );
-
     res.json({
-      message: 'Lugar de comision actualizado',
-      lugar: { ...body },
+      message: 'Registro documento actualizado',
+      registro: { ...body },
     });
   } catch (err) {
     console.log(err);
@@ -156,31 +151,35 @@ const comisionUpdate = async (req = request, res = response) => {
   }
 };
 
-const comisionDelete = async (req = request, res = response) => {
+const lugarDelete = async (req = request, res = response) => {
   const { id } = req.params;
   try {
-    const lugarComision = await LugarComision.findByPk(id);
-    if (!lugarComision) {
+    const registroDocumento = await RegistroDocumento.findByPk(id);
+    if (!registroDocumento) {
       return res.status(404).json({ message: 'El dato ingresado no existe' });
     }
-    await lugarComision.update({ estado: false });
-    res.status(200).json({ message: 'Se elimino con éxito', lugarComision });
+    console.log(registroDocumento);
+    await registroDocumento.update({ estado: false });
+    res
+      .status(200)
+      .json({ message: 'Se elimino con éxito', registroDocumento });
   } catch (err) {
+    console.log(err);
     return res.status(400).json({ message: 'Hable con el administrador', err });
   }
 };
 
-const comisionBlockDelete = (req = request, res = response) => {
+const lugarBlockDelete = (req = request, res = response) => {
   const { body } = req;
   try {
     body.map(async (element, index) => {
-      const lugarComision = await LugarComision.findByPk(element);
-      // if (!lugarComision) {
+      const registroDocumento = await RegistroDocumento.findByPk(element);
+      // if (!registroDocumento) {
       //   return res.status(404).json({ message: 'El dato ingresado no existe' });
       // }
-      await lugarComision.update({ estado: false });
+      await registroDocumento.update({ estado: false });
       if (body.length - 1 === index) {
-        res.status(200).json({ message: 'Se han eliminado con exito' });
+        res.status(200).json({ message: 'Se han eliminado con éxito' });
       }
     });
   } catch (err) {
@@ -189,11 +188,11 @@ const comisionBlockDelete = (req = request, res = response) => {
 };
 
 export {
-  comisionOne,
-  comisionAll,
-  comisionAdd,
-  comisionAddAll,
-  comisionUpdate,
-  comisionDelete,
-  comisionBlockDelete,
+  lugarOne,
+  lugarAll,
+  lugarAdd,
+  lugarUpdate,
+  lugarDelete,
+  lugarBlockDelete,
+  lugarAddAll,
 };
