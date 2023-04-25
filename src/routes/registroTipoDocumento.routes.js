c5cadbab9290a973a11f2823f424318f9e8e7390
Graciel69 <cesar95farfan@gmail.com>
2023-04-25 14:52:02 -0400

    feat: :art: new commit

--- src/routes/registroTipoDocumento.routes.js
+++ src/routes/registroTipoDocumento.routes.js
@@ -1,14 +1,19 @@
 import { Router } from 'express';
 import multer from "multer";
 import { validarJWT, haveRol } from '../middleware/index.js';
-import {addTipoDoc,getAllTipoDoc, deleteTipoDoc, getOneTipoDoc, getOneTipoDocName} from '../controllers/registroTipoDocumento.controllers.js'
+import {addTipoDoc,getAllTipoDoc, deleteTipoDoc, getOneTipoDoc, getOneTipoDocName, addTipoDocOne, updateAllTipoDoc} from '../controllers/registroTipoDocumento.controllers.js'
 const upload = multer({ dest: 'public/uploads/' });
 
 const router = Router();
 
 router.post('/registro-tipo-documento',  upload.single('file'), addTipoDoc );
 
+
+router.post('/registro-tipo-documento/one', validarJWT, haveRol('ADMIN_ROLE'),    addTipoDocOne );
+
 router.get('/tipo-documento',  validarJWT, haveRol('ADMIN_ROLE'), getAllTipoDoc );
+
+router.put('/tipo-documento/:id',  validarJWT, haveRol('ADMIN_ROLE'), updateAllTipoDoc );
 
 
 router.get('/tipo-documento/:id',  validarJWT, haveRol('ADMIN_ROLE'), getOneTipoDoc );
