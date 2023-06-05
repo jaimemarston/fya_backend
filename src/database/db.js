9f85e3d4d4de67a53600f6e9b738c9875a3203a8
michelnovellino <michelnovellino.programador@gmail.com>
2023-06-05 10:29:37 -0400

    log .env

--- src/database/db.js
+++ src/database/db.js
@@ -3,7 +3,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-
+console.log({
+  POSTGRES_DB: process.env.POSTGRES_DB,
+  POSTGRES_USER: process.env.POSTGRES_USER,
+  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+  POSTGRES_PORT: process.env.POSTGRES_PORT,
+  PORT: process.env.PORT
+})
 
 const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
   host: 'db',
